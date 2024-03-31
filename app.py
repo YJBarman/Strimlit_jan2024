e3fdcee99f7a3209e9c6c25db7338413f15eb2ac
YJBarman <93461631+YJBarman@users.noreply.github.com>
2024-03-31 19:34:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ def multiplication(a,b):
    return(a*b)
 
 st.title("Little Calculator")
-a = st.number_input()
-b = st.number_input()
+a = st.number_input("Input first number")
+b = st.number_input("Input second number")
+
+answer = multiplication(a,b)
+st.write(answer)
+
+
 
